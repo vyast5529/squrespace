ed12e5ea71484acbc7cc6401c1cefe7aa776f40c
tarunvyas <vyast5529@gmail.com>
2023-11-06 00:08:36 -0800

    first commit

---
+++ project.js
@@ -0,0 +1,56 @@
+
+gsap.from("#logo",{
+    y:-100,duration:2,delay:0.5
+})
+gsap.from("#p",{
+    y:-100,duration:2,delay:1.5
+})
+gsap.from("#t",{
+    y:-100,duration:2,delay:2.5
+})
+gsap.from("#r",{
+    y:-100,duration:2,delay:3.5
+})
+gsap.from("#l",{
+    y:-100,duration:2,delay:4.5
+})
+gsap.from("#s",{
+    y:-100,duration:2,delay:5.5
+})
+gsap.from(" #h1 ",{
+    y:-700,duration:3,delay:6.5
+})
+gsap.from(" #h2 ",{
+    y:-700,duration:3,delay:7.5
+})
+gsap.from(" #h3 ",{
+    y:-700,duration:3,delay:8.5
+})
+gsap.from("#im1",{
+    scale:0,duration:2,delay:9.5
+
+})
+gsap.from("#im2",{
+    scale:0,duration:2,delay:9.5
+
+})
+gsap.from("#im3",{
+    scale:0,duration:2,delay:9.5
+
+})
+gsap.from("#im4",{
+    scale:0,duration:2,delay:9.5
+
+})
+gsap.from("#sm",{
+    scale:0,duration:2,delay:9.5,
+})
+gsap.from("#yb",{
+    scale:0,duration:2,delay:9.5
+})
+gsap.from("#scroll",{
+    scale:0,duration:2,delay:9.5
+})
+gsap.from("#scroll",{
+    y:-20,duration:2,delay:11,repeat:-1,
+})
